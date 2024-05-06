252b698a545d3e3b05bb0e827379c4bc71fc54e6
Leonid Kuzmychenko <lleonidk@ukr.net>
2024-05-07 02:08:42 +0300

    add nginx

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
 docker volume create "pgdata"
-docker build -t "diplom-backend-ms-image" .
 docker-compose build
 docker-compose up
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker volume create "pgdata"
-docker build -t "diplom-backend-ms-image"
 docker-compose build
 docker-compose up
